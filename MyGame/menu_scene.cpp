b02ba88a4c7d01f9d0f1d8d000cedeef3f310dcf
Leon Szpilewski <leon.szpilewski@gmail.com>
2012-05-28 00:51:20 +0200

    state krempel

--- MyGame/menu_scene.cpp
+++ MyGame/menu_scene.cpp
@@ -7,15 +7,35 @@
 //
 
 #include "menu_scene.h"
+#include <vector>
 
 namespace menu_scene {
-	static void tick(double dt) {
-		printf("menu scene tick: %f\n", dt);
+	struct state_t {
+		int foo = 0;
+	};
+	
+	std::vector <state_t> states;
+	
+	 void tick(double dt, size_t state_id) {
+		state_t &state = states[state_id];
+		printf("menu scene tick: %f = %i\n", dt, state.foo);
+		state.foo++;
+		 
+		 if (state.foo > 30) {
+			 scene_manager::set_scene(create());
+		 }
 	}
-
+	
 	scene_manager::scene create() {
 		scene_manager::scene scene;
-		scene.tick_scene = tick;
+		state_t state;
+		state.foo = 23;
+		states.push_back(state);
+		size_t i = states.size()-1;
+		
+		scene.tick_scene = [i](double dt) {
+			menu_scene::tick(dt, i);
+		};
 		return scene;
 	}
 }
