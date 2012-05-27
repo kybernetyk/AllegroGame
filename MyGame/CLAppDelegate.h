7958613a8cc56a6a1dbc053941a7136af19772f1
Leon Szpilewski <leon.szpilewski@gmail.com>
2012-05-27 23:28:14 +0200

    allegro works

---
+++ MyGame/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  MyGame
+//
+//  Created by Leon Szpilewski on 5/27/12.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MyGame/CLAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CLAppDelegate.m
+//  MyGame
+//
+//  Created by Leon Szpilewski on 5/27/12.
+//
+//
+
+#import "CLAppDelegate.h"
+
+@implementation CLAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
