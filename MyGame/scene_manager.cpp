9c08556f2eebde4e68329d5040b012039201567f
Leon Szpilewski <leon.szpilewski@gmail.com>
2012-05-28 00:11:26 +0200

    scene fun

--- MyGame/scene_manager.cpp
+++ MyGame/scene_manager.cpp
@@ -23,7 +23,11 @@ namespace scene_manager {
 		if (current_scene.draw_scene)
 			current_scene.draw_scene(dt);
 		
+		//change to scene which was set in set_scene
 		if (next_scene.run_scene != nullptr) {
+			if (current_scene.destroy_scene)
+				current_scene.destroy_scene();
+			
 			current_scene = next_scene;
 			next_scene.run_scene = nullptr;
 			
